4d21bab03d94f8523e4f3dcca3b74baba780aea7
Pablo Rodrigo <pablo_rodrigo10@hotmail.com>
2020-03-25 22:22:22 -0300

    Testing routes creation

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,25 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json()); //wll pass a json in a body of my requests
 
-app.get('/', (request,response)=>{
+app.get('/rota', (request,response)=>{
+    const qParams = request.query; //url params
+    const body = request.body; // 
+    console.log(body);
     response.json({
         nome: 'Pablo Rodrigo',
-        trigger: 'Omnistack Week'
+        trigger: 'Omnistack Week',
+        params: qParams
+    });
+});
+
+app.get('/rota/:id', (request,response)=>{
+    const rParams = request.params; // route params
+    response.json({
+        nome: 'Pablo Rodrigo',
+        trigger: 'Omnistack Week',
+        params: rParams
     });
 });
 
