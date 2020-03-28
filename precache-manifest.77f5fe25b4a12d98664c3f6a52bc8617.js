4c9435b3bc75adba646e1feb050f363d2c07bf2e
Pablo Rodrigo <pablo_rodrigo10@hotmail.com>
2020-03-28 13:39:43 -0300

    Updates

---
+++ precache-manifest.77f5fe25b4a12d98664c3f6a52bc8617.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7751226bd8338f6a4e20e4412a427ce9",
+    "url": "/omnistack-week/frontend/index.html"
+  },
+  {
+    "revision": "32e6782c980ec5701e02",
+    "url": "/omnistack-week/frontend/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "370c9fd060cfe3a4ac85",
+    "url": "/omnistack-week/frontend/static/js/2.6d20342a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/omnistack-week/frontend/static/js/2.6d20342a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32e6782c980ec5701e02",
+    "url": "/omnistack-week/frontend/static/js/main.b75239b6.chunk.js"
+  },
+  {
+    "revision": "a1cc073cb59d52380a2f",
+    "url": "/omnistack-week/frontend/static/js/runtime-main.d4bdd0f0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/omnistack-week/frontend/static/media/logo.5d5d9eef.svg"
+  }
+]);
